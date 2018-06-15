5f3cc25f14de634be1b808e765f525cab2d4be05
luikhh <1292092140@qq.com>
2018-06-15 23:09:47 +0800

    Update hznu.js

--- hznu.js
+++ hznu.js
@@ -11,12 +11,12 @@ for (var i=0; i<8; i++) {
 var script = document.createElement('script');
 script.src = '//code.jquery.com/jquery-3.3.1.js';
 document.body.appendChild(script);
-$('#iframeautoheight').contents().find('select').removeAttr("disabled").val("好");
-$('#iframeautoheight').contents().find('select:last').removeAttr("disabled").val("较好");
-$('#iframeautoheight').contents().find('#Button1').click()
+$('#iframeautoheight').contents().find('select').removeAttr("disabled").val("优秀");
+$('#iframeautoheight').contents().find('select:last').removeAttr("disabled").val("良好");
+$('#iframeautoheight').contents().find('#Button1').click()//点击保存按钮
 
 
-$('#iframeautoheight').contents().find('select:first').removeAttr("disabled").val("较好");
+$('#iframeautoheight').contents().find('select:first').removeAttr("disabled").val("良好");
 
 
 
